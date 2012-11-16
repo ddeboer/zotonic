ddeboer/zotonic

commit 50e0c1d5e429780466d97b14e7f18133dec30a01

core: add guards to m_rsc_gone functions.
This gives a better error message.

Fix for controller_page:previously_existed/2 to return 404 for unknown resource id's.

Fixes #459.

Also an additional fix for controller_page:resource_exists/2,
as discussed in commit 45bca641 on github.

src/models/m_rsc_gone.erl
@@ -54,7 +54,7 @@ m_value(#m{value=undefined}, _Context) ->
 
 
 %% @doc Get the possible 'rsc_gone' resource for the id.
-get(Id, Context) ->
+get(Id, Context) when is_integer(Id) ->
 	F = fun() ->
 			z_db:assoc_row("select * from rsc_gone where id = $1", [Id], Context) 
 		end,
@@ -63,7 +63,7 @@ get(Id, Context) ->
 
 %% @doc Check if the resource used to exist.
 -spec is_gone(integer(), #context{}) -> boolean().
-is_gone(Id, Context) ->
+is_gone(Id, Context) when is_integer(Id) ->
 	F = fun() ->
 			z_db:q1("select count(*) from rsc_gone where id = $1", [Id], Context) =:= 1
 		end,
@@ -71,12 +71,12 @@ is_gone(Id, Context) ->
 
 %% @doc Copy a resource to the 'gone' table, use the current user as the modifier (deleter).
 -spec gone(integer(), #context{}) -> {ok, integer()}.
-gone(Id, Context) ->
+gone(Id, Context) when is_integer(Id) ->
 	gone(Id, undefined, Context).
 
 %% @doc Copy a resource to the 'gone' table, use the current user as the modifier (deleter).
 %%      Also sets the 'new id', which is the id that replaces the deleted id.
-gone(Id, NewId, Context) ->
+gone(Id, NewId, Context) when is_integer(Id), is_integer(NewId) ->
 	Props = z_db:assoc_row("
 			select id, name, version, page_path, uri, is_authoritative, creator_id, created
 			from rsc

modules/mod_base/controllers/controller_page.erl
@@ -34,12 +34,13 @@ resource_exists(ReqData, Context) ->
     ContextQs = z_context:ensure_qs(Context1),
     try
         Id = get_id(ContextQs),
-        true = m_rsc:exists(Id, ContextQs),
-        case z_context:get(cat, ContextQs) of
-            undefined ->
-                ?WM_REPLY(true, ContextQs);
-            Cat ->
-                ?WM_REPLY(m_rsc:is_a(Id, Cat, ContextQs), ContextQs)
+        case {m_rsc:exists(Id, ContextQs), z_context:get(cat, ContextQs)} of
+            {Exists, undefined} ->
+                ?WM_REPLY(Exists, ContextQs);
+            {true, Cat} ->
+                ?WM_REPLY(m_rsc:is_a(Id, Cat, ContextQs), ContextQs);
+            {false, _} ->
+                ?WM_REPLY(false, ContextQs)
         end
     catch
         _:_ -> ?WM_REPLY(false, ContextQs)
@@ -48,7 +49,11 @@ resource_exists(ReqData, Context) ->
 %% @doc Check if the resource used to exist
 previously_existed(ReqData, Context) ->
     Context1 = ?WM_REQ(ReqData, Context),
-    IsGone = m_rsc_gone:is_gone(get_id(Context1), Context1),
+    IsGone = case get_id(Context1) of
+                 Id when is_integer(Id) ->
+                     m_rsc_gone:is_gone(Id, Context1);
+                 _ -> false
+             end,
     ?WM_REPLY(IsGone, Context1).
 
 

commit 099c8b4f2b617cf9b1963c1cdb92fad8f69fcd39

core: Fix deletion of resources.
@kaos broke it in 50e0c1d5e429780466d97b14e7f18133dec30a01

src/models/m_rsc_gone.erl
@@ -76,7 +76,7 @@ gone(Id, Context) when is_integer(Id) ->
 
 %% @doc Copy a resource to the 'gone' table, use the current user as the modifier (deleter).
 %%      Also sets the 'new id', which is the id that replaces the deleted id.
-gone(Id, NewId, Context) when is_integer(Id), is_integer(NewId) ->
+gone(Id, NewId, Context) when is_integer(Id), is_integer(NewId) orelse NewId =:= undefined ->
 	Props = z_db:assoc_row("
 			select id, name, version, page_path, uri, is_authoritative, creator_id, created
 			from rsc